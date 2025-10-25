b6d5f1fb73a12db31543a3ab42ee3eff560920f8
yusuf <yusufbbb444@gmail.com>
2025-10-25 21:44:18 +0700

    update quiz

---
+++ app/(root)/quiz/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import Bj from "@/components/Bannerjudul";
+import Footer from "@/components/Ftr";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const path = usePathname();
+  const isBeritaPage = path.includes("/news/berita");
+
+  return (
+    <>
+      <Navbar />
+      <Bj title="Quiz" />
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
+}
